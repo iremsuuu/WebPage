84a9991ce4711587ee72307b6888cab420261d2b
iremsu namkoç <iremsunamkoc@hotmail.com>
2024-01-24 14:00:49 +0300

    Proje dosyası ekle.

---
+++ WebPage/Pages/HomePage.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebPage.Pages
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Sayfa yüklendiðinde yapýlacak iþlemler
+        }
+    }
+}
